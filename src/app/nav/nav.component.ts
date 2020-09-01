4884495f7993b1aa2a142e9dd42445b2c1b77d99
zhen <kelly811124811124@hotmail.com>
2020-09-01 18:10:29 +0800

    nav design

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-nav',
@@ -6,10 +6,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./nav.component.scss']
 })
 export class NavComponent implements OnInit {
+  menu = ['Start', 'About Me', 'Skill Tree', 'Github'];
+  nowIndex: number;
+  nowPage: number;
 
+  @HostListener('window:scroll', ['$event'])
+  onScroll(event) {
+    this.nowPage = this.getPage();
+  }
   constructor() { }
 
   ngOnInit(): void {
+    this.nowPage = this.getPage();
   }
 
+  enter(index) {
+    this.nowIndex = index;
+  }
+
+  out() {
+    this.nowIndex = null;
+  }
+
+  getPage() {
+    return Math.floor(scrollY / window.innerHeight);
+  }
+
+  toPage(i) {
+    window.scrollTo({
+      top: window.innerHeight * i,
+      behavior: 'smooth'
+    });
+  }
 }
